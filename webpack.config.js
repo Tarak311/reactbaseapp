add1c88f7ff8b7340fea9bece841c99ce619b686
Tarak311 <tarak_1@live.com>
2021-01-27 23:02:50 -0500

    new test

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    },
-    plugins: [new HtmlWebpackPlugin()],
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 9000
     },
     module: {
         rules: [
